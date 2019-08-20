bb6e6a9b6feb7f126732b2b27443035b8e99139b
Clinton Graham <ctgraham@pitt.edu>
2019-08-20 16:31:58 -0400

    #43: move book save failure notification to be effective

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -81,6 +81,7 @@ class BooksController extends AppController
                         }
                     }
                 } else {
+                    $this->Flash->error(__('The book could not be saved. Please, try again.'));
                     // Prevent to insert the book in database.
                     return $this->redirect(['action' => 'scan',
                                             'tray_id' => $book['tray_id'],
@@ -90,7 +91,6 @@ class BooksController extends AppController
                                             'block_item' => $book->book_barcode]);
                 }
             }
-            $this->Flash->error(__('The book could not be saved. Please, try again.'));
         }
         $this->set(compact('book'));
     }
